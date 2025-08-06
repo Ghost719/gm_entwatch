cfd961428363b1bf094d1c3ece3a8d59292be8f6
Vladimir <vladimir.ghost719@yandex.ru>
2025-08-06 19:23:43 +0300

    Adding map configs

---
+++ lua/entwatch/maps/ze_bioluminescent_b2.lua
@@ -0,0 +1,12 @@
+return {
+    {
+        ["name"] = "Heal",
+        ["shortname"] = "Heal",
+        ["filtername"] = "human_heal",
+        ["hammerid"] = 1794602,
+        ["buttonid"] = 1794595,
+        ["mode"] = ENTWATCH_MODE_COOLDOWNS,
+        ["cooldown"] = 80,
+        ["pt_spawner"] = "human_heal_itemtemp",
+    },
+}
