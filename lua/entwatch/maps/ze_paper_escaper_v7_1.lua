79f076dd522644d62d8c607f53d0acef0953a956
Vladimir <vladimir.ghost719@yandex.ru>
2025-08-11 03:04:22 +0300

    Add ze_paper_escaper_v7_1 and ze_quiver_v2a configs

---
+++ lua/entwatch/maps/ze_paper_escaper_v7_1.lua
@@ -0,0 +1,12 @@
+return {
+    {
+        ["name"] = "Heal",
+        ["shortname"] = "Heal",
+        ["filtername"] = "player_heal",
+        ["hammerid"] = 756928,
+        ["buttonid"] = 757014,
+        ["mode"] = ENTWATCH_MODE_COOLDOWNS,
+        ["cooldown"] = 80,
+        ["pt_spawner"] = "heal_spawn",
+    },
+}
---
+++ lua/entwatch/maps/ze_quiver_v2a.lua
@@ -0,0 +1,22 @@
+return {
+    {
+        ["name"] = "Ammo Refill",
+        ["shortname"] = "Ammo",
+        ["filtername"] = "weapon_ammorefill_player",
+        ["hammerid"] = 466464,
+        ["buttonid"] = 466470,
+        ["mode"] = ENTWATCH_MODE_COOLDOWNS,
+        ["cooldown"] = 35,
+        ["pt_spawner"] = "weapon_ammorefill_location_template",
+    },
+    {
+        ["name"] = "Ice",
+        ["shortname"] = "Ice",
+        ["filtername"] = "weapon_ice_player",
+        ["hammerid"] = 466620,
+        ["buttonid"] = 466624,
+        ["mode"] = ENTWATCH_MODE_COOLDOWNS,
+        ["cooldown"] = 35,
+        ["pt_spawner"] = "weapon_ice_location_template",
+    },
+}
